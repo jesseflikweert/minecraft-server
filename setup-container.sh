850a7f108ca521aefe3ef3902f3eb2ee30a671ad
fl1k <jpflikweert@gmail.com>
2023-04-01 13:09:06 +0200

    Testing some basic init

--- setup-container.sh
+++ setup-container.sh
@@ -2,10 +2,11 @@
 
 # get the container ID and password from the command-line arguments
 container_id=$1
-password=$2
+container_password=$2
+mrcron_password=$3
 
-if [ -z "$container_id" ] || [ -z "$password" ]; then
-    echo "Usage: $0 <container_id> <password>"
+if [ -z "$container_id" ] || [ -z "$container_password" ]  || [ -z "$mrcron_password" ]; then
+    echo "Usage: $0 <container_id> <container_password> <mrcron_password>"
     exit 1
 fi
 
@@ -16,7 +17,7 @@ pveam update
 pveam download local debian-11-standard_11.6-1_amd64.tar.zst
 
 # create a new LXC container with the specified ID and password
-pct create $container_id local:vztmpl/debian-11-standard_11.6-1_amd64.tar.zst --hostname debian-minecraft --storage local-lvm --rootfs 20 --cores 4 --memory 6144 --net0 name=eth0,bridge=vmbr0,ip=dhcp --password $password --unprivileged --features nesting=1,keyctl=1 --onboot 1
+pct create $container_id local:vztmpl/debian-11-standard_11.6-1_amd64.tar.zst --hostname debian-minecraft --storage local-lvm --rootfs 100 --cores 4 --memory 6144 --net0 name=eth0,bridge=vmbr0,ip=dhcp --password $container_password --unprivileged --features nesting=1,keyctl=1 --onboot 1
 
 # check if the container was created successfully
 if [ $? -ne 0 ]; then
@@ -35,9 +36,8 @@ pct exec $container_id -- bash -c "apt-get update"
 pct exec $container_id -- bash -c "apt-get upgrade -y"
 
 # install required packages inside the container
-pct exec $container_id -- bash -c "apt-get install -y nano openjdk-17-jre-headless git"
+pct exec $container_id -- bash -c "apt-get install -y nano"
 
-# clone the GitHub repository inside the container
-pct exec $container_id -- bash -c "git clone https://github.com/jesseflikweert/minecraft-server.git /root/minecraft-server"
+# initialise server
+pct exec $container_id -- bash -c "$(wget -qLO - https://github.com/jesseflikweert/minecraft-server/initialise-server.sh)"
 
-echo "Done."
